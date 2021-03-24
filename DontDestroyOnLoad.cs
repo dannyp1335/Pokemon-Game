81085b921176f76b5aa66fd6c3eb0624263e41d7
dannyp1335 <61282541+dannyp1335@users.noreply.github.com>
2021-03-24 03:00:43 -0400

    Load(Dont Destroy)

---
+++ DontDestroyOnLoad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+    public GameObject pokemon;
+    public string pokeName;
+
+	// Use this for initialization
+	void Start () {
+        pokeName = GetComponent<PokemonHoverOver>().getName();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(pokemon.name.Equals(pokeName))
+        {
+            DontDestroyOnLoad(pokemon);
+        }
+
+		
+	}
+}
