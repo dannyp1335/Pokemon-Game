587c62fa5fff1c4f05c853a86f39d7653d6f3615
dannyp1335 <61282541+dannyp1335@users.noreply.github.com>
2021-03-24 03:13:19 -0400

    Pokemon Storage

---
+++ BasePokemonDataStorage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class BasePokemonDataStorage
+{
+    public string name;
+    public Stat maxHealth;
+    public Stat currentHealth;
+    public Stat attack;
+    public Stat defense;
+    public Stat spAttack;
+    public Stat spDefense;
+    public Stat speed;
+    public string type;
+    public string type2;
+    public int baseExperienceYield;
+
+    public bool fainted;
+
+    public bool[] conditions;
+
+    public int level;
+
+    public int currentExp;
+    public int experienceToNextLevel;
+    public int evolveLevel;
+}
