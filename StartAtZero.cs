a1ba186ccd37da2982b4e6b47c2b3eeb3ef66cf0
dannyp1335 <61282541+dannyp1335@users.noreply.github.com>
2021-03-24 02:59:03 -0400

    Start at Zero

---
+++ StartAtZero.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAtZero : MonoBehaviour {
+
+    public GameObject pokeSpawn;
+
+	// Use this for initialization
+	void Start () {
+        //pokeSpawn = GetComponent<PokemonHoverOver>().getGameObject();
+        Vector3 newPosition = new Vector3(0, 0, 0);
+       pokeSpawn.transform.position = newPosition;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
