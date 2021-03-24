e4d2bb03591f3162c3491b2d4dae9bf85aec150b
dannyp1335 <61282541+dannyp1335@users.noreply.github.com>
2021-03-24 03:13:00 -0400

    Inventory Bar

---
+++ Bar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//this class cannot be a monobehaviour
+public class Bar
+{
+    [SerializeField]
+    private float fillAmount;
+    [SerializeField]
+    private Image image;
+
+    //constructor
+    public Bar(Image tempImage, float tempFillAmount)
+    {
+        image = tempImage;
+        fillAmount = tempFillAmount;
+    }
+
+    //converts health value into a number that can be understood by fillamount (only takes vales between 0 and 1)
+    public void setHealthBar(float health, float maximumHealth)
+    {
+        fillAmount = health / maximumHealth;
+        image.fillAmount = fillAmount;
+    }
+}
