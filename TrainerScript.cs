c2a0ed5f166d4ad21a4b4ae585a0ba245612a4fd
dannyp1335 <61282541+dannyp1335@users.noreply.github.com>
2021-03-24 02:59:23 -0400

    Trainer Info

---
+++ TrainerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainerScript : MonoBehaviour
+{
+
+    public GameObject trainer;
+    public bool trainerLife;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        trainerLife = true;
+        Debug.Log(trainerLife);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void trainerCoolDown()
+    {
+        trainerLife = BattleManager.trainerBool;
+        Debug.Log(trainerLife);
+        if (BattleManager.trainerBool == false)
+        {
+            Debug.Log(BattleManager.trainerBool);
+            trainer.GetComponent<PokemonHoverOver>().ifTrainerLost();
+            Invoke("trainerHealed", 10);
+            trainerLife = true;
+            BattleManager.trainerBool = true;
+            Debug.Log(BattleManager.trainerBool);
+            Debug.Log(trainerLife);
+        }
+    }
+    public void trainerHealed()
+    {
+        trainer.GetComponent<PokemonHoverOver>().doFadeText();
+    }
+
+}
