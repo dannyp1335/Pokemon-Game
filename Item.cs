f296586452422afc01ff2b90198bba972d7f132c
dannyp1335 <61282541+dannyp1335@users.noreply.github.com>
2021-03-24 03:14:07 -0400

    Item

---
+++ Item.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item 
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private int number;
+    [SerializeField]
+    private string description;
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public string getDescription()
+    {
+        return description;
+    }
+    
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(int num)
+    {
+        number = num;
+    }
+
+    public void setNameText(string name, Text nText)
+    {
+        nText.text = name;
+    }
+
+    public void setNumberText(int? num, Text nText)
+    {
+        nText.text = num.ToString();
+    }
+
+    public void setXText(string x,Text xText)
+    {
+        xText.text = x;
+    }
+    public void setDescriptionText(string description, Text dText)
+    {
+        dText.text = description;
+    }
+}
